5b4674fb79e1ddbc424ae9ac5d0369611e95e043
Victorcreed07 <79251777+Victorcreed07@users.noreply.github.com>
2021-07-18 12:07:53 +0530

    Create Formula.js

---
+++ Formula.js
@@ -0,0 +1,7 @@
+import React from "react"
+class Formula extends React.Component {
+  render() {
+    return <div className="formulaScreen">{this.props.formula}</div>;
+  }
+}
+export default Formula
